408081a118678b91c5ec882e552fb0b5631b1395
eugene.levenetc <eugene.levenetc@themobilecompany.com>
2019-06-07 11:57:42 +0200

    Update db scheme: replace int with uuid

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -4,14 +4,14 @@ create table users
   email         varchar(320),
   password      varchar(255),
   password_salt varchar(255),
-  id            int not null auto_increment primary key
+  id            binary(16) not null primary key
 );
 
 create table motorcycles
 (
-  name     varchar(255),
-  id       int not null auto_increment primary key,
-  user_id  int not null,
+  name    varchar(255),
+  id      binary(16) not null primary key,
+  user_id binary(16) not null,
   constraint motorcycles_users_fk
     foreign key (user_id)
       references users (id)
@@ -19,11 +19,11 @@ create table motorcycles
 
 create table locations
 (
-  id            int not null auto_increment primary key,
+  id            binary(16) not null primary key,
   lat           double,
   lon           double,
   date          long,
-  motorcycle_id int not null,
+  motorcycle_id binary(16) not null,
   constraint locations_motorcycles_fk
     foreign key (motorcycle_id)
       references motorcycles (id)
@@ -31,10 +31,10 @@ create table locations
 
 create table access_tokens
 (
-  id      int not null auto_increment primary key,
+  id      binary(16) not null primary key,
   date    long,
   value   varchar(255),
-  user_id int not null,
+  user_id binary(16) not null,
   constraint access_tokens_users_fk
     foreign key (user_id)
       references users (id)
